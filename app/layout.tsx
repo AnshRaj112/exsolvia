df659da6bdd5da8316c94b1d6163295862e20fc1
AnshRaj112 <anshraj112@gmail.com>
2026-01-21 15:29:56 +0530

    Removing snowflakes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "./components/footer";
-import Snowflakes from "./components/snowflakes";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +28,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Snowflakes />
         <main>
           {children}
         </main>
