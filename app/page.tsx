4256162092cd9836d89c143631670b2044dbb3da
AnshRaj112 <anshraj112@gmail.com>
2026-03-15 02:09:31 +0530

    Updating shit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "./components/hero";
+import ScrollReveal from "./components/scroll-reveal";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <ScrollReveal />
     </main>
   );
 }
